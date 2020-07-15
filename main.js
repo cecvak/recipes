b34e4460f46ed8094ce2e8916cf6a660851951bc
“cecvak” <c.vakaloudi@gmail.com>
2020-07-15 11:45:10 +0200

    change json file. need to make all buttons work as intended

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
-$(document).ready(function(){
-    $(".btn-primary:first").click(function(){
-      $(this).button('toggle');
-    });   
-  });
+// $(document).ready(function(){
+//     $(".btn-primary:first").click(function(){
+//       $(this).button('toggle');
+//     });   
+//   });
+
+
+const app = new Vue ({
+    el: '#vue-app1',
+    data: {
+        recipe: [],
+        randomRecipe: {},
+    },
+
+    methods: {
+        random() {
+            const someRecipe = Math.floor(Math.random() * this.recipe.length);
+            this.randomRecipe = this.recipe[someRecipe];
+            console.log(this.randomRecipe);
+        }
+    },
+
+    created: function () {
+        fetch('recipe2.json')
+            .then(response => response.json())
+            .then(recipeResp => {
+                this.recipe = recipeResp.recipes;
+                console.log(recipeResp.recipes)
+            })
+        //     this.recipe = recipes;
+        //     console.log(recipe)
+        // }
+        
+    },
+    
+})
